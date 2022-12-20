1192e756b09d8b0d64a9b50292c1b19aedd254d7
ixoracoin <104260343+ixoracoin@users.noreply.github.com>
2022-12-20 16:08:06 +0330

    Update countDown.js

--- js/countDown.js
+++ js/countDown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("February 01, 2023 14:30:00").getTime();
+var countDownDate = new Date("January 01, 2023 14:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
